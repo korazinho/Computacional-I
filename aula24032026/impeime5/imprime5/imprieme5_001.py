4aede2bfe34fb596c548aa742db1238e8411ecf2
korazinho <felipaocora2@gmail.com>
2026-03-24 16:49:46 -0300

    Add files via upload

---
+++ aula24032026/impeime5/imprime5/imprieme5_001.py
@@ -0,0 +1,21 @@
+"""
+Programa imprime5 
+Descrição: Este programa imprime na tela os 5 primeiros numeros naturais
+Autor: Felipe F. Corá
+Data: 24/03/26
+Versão: 0.0.1
+"""
+# Alocação de memória
+numero_1 = 0
+numero_2 = 0
+numero_3 = 0
+numero_4 = 0
+numero_5 = 0
+i = 0
+# Entrada de dados
+# Processamento de dados
+# Saída de dados
+while i < 5:
+    print(i+1)
+    i=i+1
+    
---
+++ aula24032026/impeime5/imprime5/imprieme5_010.py
@@ -0,0 +1,17 @@
+"""
+Programa imprime5 
+Descrição: Este programa imprime na tela os 5 primeiros numeros naturais
+Autor: Felipe F. Corá
+Data: 24/03/26
+Versão: 0.1.0
+"""
+# Alocação de memória
+
+i = 0
+# Entrada de dados
+# Processamento de dados
+# Saída de dados
+while i < 5:
+    print(i+1)
+    i=i+1
+    
